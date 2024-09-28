5a1b80422383a3df0e36eb6dbdaf220198e238c5
BhaktiSojitra <sojitrabhakti03@gmail.com>
2024-09-28 22:44:45 +0530

    Add files via upload

---
+++ CommonVariable.cs
@@ -0,0 +1,36 @@
+﻿namespace CRUD_Demo
+{
+    public class CommonVariable
+    {
+        private static IHttpContextAccessor _HttpContextAccessor;
+        static CommonVariable()
+        {
+            _HttpContextAccessor = new HttpContextAccessor();
+        }
+        public static int? UserID()
+        {
+            if (_HttpContextAccessor.HttpContext.Session.GetString("UserID") == null)
+            {
+                return null;
+            }
+            return Convert.ToInt32(_HttpContextAccessor.HttpContext.Session.GetString("UserID"));
+        }
+        public static string UserName()
+        {
+            if (_HttpContextAccessor.HttpContext.Session.GetString("UserName") == null)
+            {
+                return null;
+            }
+
+            return _HttpContextAccessor.HttpContext.Session.GetString("UserName");
+        }
+        public static string Email()
+        {
+            if (_HttpContextAccessor.HttpContext.Session.GetString("Email") == null)
+            {
+                return null;
+            }
+            return _HttpContextAccessor.HttpContext.Session.GetString("Email");
+        }
+    }
+}
