a7dd5817269785642e6ad3e2ae4a5bb60bf013f2
Bhakti Sojitra <sojitrabhakti03@gmail.com>
2024-12-21 23:18:52 +0530

    Add files via upload

---
+++ Help Folder/Stored Procedures/Order_Procedure.sql
@@ -0,0 +1,129 @@
+-- select all
+CREATE PROCEDURE  [dbo].[PR_Order_SelectAll]
+AS
+BEGIN
+	SELECT 
+		   [dbo].[LOC_Order].[OrderID],
+		   [dbo].[LOC_Order].[OrderNumber],
+		   [dbo].[LOC_Order].[OrderDate],
+		   [dbo].[LOC_Order].[CustomerID],
+		   [dbo].[LOC_Order].[PaymentMode],
+		   [dbo].[LOC_Order].[TotalAmount],
+		   [dbo].[LOC_Order].[ShippingAddress],
+		   [dbo].[LOC_Order].[UserID]
+	FROM [dbo].[LOC_Order]
+	INNER JOIN [dbo].[LOC_Customer] 
+	ON [dbo].[LOC_Order].[CustomerID] = [dbo].[LOC_Customer].[CustomerID]
+	INNER JOIN [dbo].[LOC_User] 
+	ON [dbo].[LOC_Order].[UserID] = [dbo].[LOC_User].[UserID]
+	ORDER BY [dbo].[LOC_Order].[OrderDate],
+			 [dbo].[LOC_Order].[PaymentMode],
+		     [dbo].[LOC_Order].[TotalAmount],
+		     [dbo].[LOC_Order].[ShippingAddress];
+END
+
+EXEC [dbo].[PR_Order_SelectAll] 
+
+
+-- by primary key
+ALTER PROCEDURE [dbo].[PR_Order_SelectByPK]
+	@OrderID int
+AS
+BEGIN
+	SELECT 
+		   [dbo].[LOC_Order].[OrderID],
+		   [dbo].[LOC_Order].[OrderNumber],
+		   [dbo].[LOC_Order].[OrderDate],
+		   [dbo].[LOC_Order].[CustomerID],
+		   [dbo].[LOC_Order].[PaymentMode],
+		   [dbo].[LOC_Order].[TotalAmount],
+		   [dbo].[LOC_Order].[ShippingAddress],
+		   [dbo].[LOC_Order].[UserID]
+	FROM [dbo].[LOC_Order] 
+	WHERE [dbo].[LOC_Order].[OrderID] = @OrderID;
+END
+
+exec [dbo].[PR_Order_SelectByPK] 3 
+
+
+-- insert
+ALTER PROCEDURE [dbo].[PR_Order_Insert]
+	@OrderNumber INT,
+    @OrderDate DATETIME,
+    @CustomerID INT,
+    @PaymentMode VARCHAR(100),
+    @TotalAmount DECIMAL(10,2),
+    @ShippingAddress VARCHAR(100),
+    @UserID INT
+AS
+BEGIN
+    INSERT INTO [dbo].[LOC_Order] 
+	(
+		[OrderNumber],
+        [OrderDate],
+        [CustomerID],
+        [PaymentMode],
+        [TotalAmount],
+        [ShippingAddress],
+        [UserID]
+    )
+    VALUES 
+	(
+		@OrderNumber,
+        @OrderDate,
+        @CustomerID,
+        @PaymentMode,
+        @TotalAmount,
+        @ShippingAddress,
+        @UserID
+    );
+END
+
+EXEC [dbo].[PR_Order_Insert] 4,'08-09-2024 09:50 PM' , 2 ,'Credit Card' , 250.00 , 'line 3' , 3
+EXEC [dbo].[PR_Order_SelectAll] 
+
+
+-- update
+CREATE PROCEDURE [dbo].[PR_Order_UpdateByPK]
+	@OrderID int,
+	@OrderDate DATETIME,
+    @PaymentMode VARCHAR(100),
+    @TotalAmount DECIMAL(10,2),
+    @ShippingAddress VARCHAR(100)
+AS
+BEGIN
+    UPDATE [dbo].[LOC_Order]
+    SET [OrderDate] = @OrderDate,
+		[PaymentMode] = @PaymentMode,
+        [TotalAmount] = @TotalAmount,
+        [ShippingAddress] = @ShippingAddress
+    WHERE [dbo].[LOC_Order].[OrderID] = @OrderID;
+END
+
+EXEC [dbo].[PR_Order_UpdateByPK] 4,'08-09-2024 10:22PM',1,'Pay Pal',200.00,'line 2',3
+EXEC [dbo].[PR_Order_SelectAll] 
+
+
+-- delete
+CREATE PROCEDURE [dbo].[PR_Order_DeleteByPK] 
+	@OrderID int 
+AS
+BEGIN
+	DELETE 
+	FROM [dbo].[LOC_Order] 
+	WHERE [dbo].[LOC_Order].[OrderID] = @OrderID;
+END
+
+EXEC [dbo].[PR_Order_DeleteByPK] 4
+EXEC [dbo].[PR_Order_SelectAll] 
+
+
+-- dropdown
+CREATE PROCEDURE [dbo].[PR_Order_DropDown]
+AS
+BEGIN
+	SELECT 
+		OrderID , 
+		OrderNumber 
+	FROM [dbo].[LOC_Order];
+END
---
+++ Help Folder/Stored Procedures/Product_Procedure.sql
@@ -0,0 +1,116 @@
+-- select all
+CREATE PROCEDURE [dbo].[PR_Product_SelectAll]
+AS
+BEGIN
+	SELECT 
+		   [dbo].[LOC_Product].[ProductID],
+		   [dbo].[LOC_Product].[ProductName],
+		   [dbo].[LOC_Product].[ProductPrice],
+		   [dbo].[LOC_Product].[ProductCode],
+		   [dbo].[LOC_Product].[Description],
+		   [dbo].[LOC_Product].[UserID] 
+	FROM [dbo].[LOC_Product]
+	INNER JOIN [dbo].[LOC_User] 
+	ON [dbo].[LOC_Product].[UserID] = [dbo].[LOC_User].[UserID]
+	ORDER BY [dbo].[LOC_Product].[ProductName],
+			 [dbo].[LOC_Product].[ProductPrice],
+		     [dbo].[LOC_Product].[ProductCode],
+		     [dbo].[LOC_Product].[Description];
+END
+
+EXEC [dbo].[PR_Product_SelectAll] 
+
+
+-- by primary key
+ALTER PROCEDURE [dbo].[PR_Product_SelectByPK]
+	@ProductID int
+AS
+BEGIN
+	SELECT 
+		   [dbo].[LOC_Product].[ProductID],
+		   [dbo].[LOC_Product].[ProductName],
+		   [dbo].[LOC_Product].[ProductPrice],
+		   [dbo].[LOC_Product].[ProductCode],
+		   [dbo].[LOC_Product].[Description],
+		   [dbo].[LOC_Product].[UserID]
+	FROM [dbo].[LOC_Product] 
+	WHERE [dbo].[LOC_Product].[ProductID] = @ProductID;
+END
+
+EXEC [dbo].[PR_Product_SelectByPK] 3 
+
+
+-- insert
+CREATE PROCEDURE [dbo].[PR_Product_Insert]
+	@ProductName varchar(100),
+	@ProductPrice decimal(10,2),
+	@ProductCode varchar(100),
+	@Description varchar(100),
+	@UserID int
+AS
+BEGIN
+	INSERT INTO [dbo].[LOC_Product] 
+	(
+		[ProductName],
+		[ProductPrice],
+		[ProductCode],
+		[Description],
+		[UserID] 
+	)
+	VALUES 
+	(
+		@ProductName,
+		@ProductPrice,
+		@ProductCode,
+		@Description,
+		@UserID 
+	);
+END
+
+EXEC [dbo].[PR_Product_Insert] 'Product C', 22.00, 'C003', 'Description for Product C', 3
+EXEC [dbo].[PR_Product_SelectAll] 
+
+
+-- update
+CREATE PROCEDURE [dbo].[PR_Product_UpdateByPK]
+    @ProductID INT,
+    @ProductName VARCHAR(100),
+    @ProductPrice DECIMAL(10,2),
+    @ProductCode VARCHAR(100),
+    @Description VARCHAR(100)
+AS
+BEGIN
+    UPDATE [dbo].[LOC_Product]
+    SET [ProductName] = @ProductName,
+        [ProductPrice] = @ProductPrice,
+        [ProductCode] = @ProductCode,
+        [Description] = @Description
+    WHERE [dbo].[LOC_Product].[ProductID] = @ProductID;
+END;
+
+EXEC [dbo].[PR_Product_UpdateByPK] 4 , 'Product D' , 19.00 , D005 , 'Description D' , 
+EXEC [dbo].[PR_Product_SelectAll] 
+
+
+-- delete
+CREATE PROCEDURE [dbo].[PR_Product_DeleteByPK] 
+	@ProductID int 
+AS
+BEGIN
+	DELETE 
+	FROM [dbo].[LOC_Product] 
+	WHERE [dbo].[LOC_Product].[ProductID] = @ProductID
+END
+
+EXEC [dbo].[PR_Product_DeleteByPK] 4
+EXEC [dbo].[PR_Product_SelectAll] 
+
+-- dropdown
+CREATE PROCEDURE [dbo].[PR_Product_DropDown]
+AS
+BEGIN
+	SELECT 
+		ProductID , 
+		ProductName 
+	FROM [dbo].[LOC_Product];
+END
